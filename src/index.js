018042e48f1a0a1b5fc29b14a91d2534e0c2c003
denkquer <lukas.thiemann@gmail.com>
2019-08-20 21:24:05 +0200

    added url params and fixed src tree

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 /*global */
 document.body.style.margin = '0px';
-
-let matrix = new Matrix(10);
+let matrix;
 
 function setup() {
   createCanvas(innerWidth, innerHeight);
+
+  let params = new URLSearchParams(document.location.search);
+  matrix = new Matrix(100);
+  if (typeof parseInt(params.get('knots')) === 'number')
+    if (Math.abs(params.get('knots')) < 300) {
+      console.log(Math.abs(params.get('knots')));
+      matrix = new Matrix(Math.abs(params.get('knots')));
+    }
   //noLoop();
 
   // matrix.dots.push(new Dot(100, 100, '0'));
@@ -23,12 +30,15 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-function Dot(initialX, initialY, debug = undefined) {
+function Dot(initialX, initialY, i = NaN, debug = undefined) {
+  this.index = i;
   this.debug = debug;
   this.x = initialX;
   this.y = initialY;
+  this.toleranz = 1;
   this.color = [255, 255, 255];
   this.nachbarn = [];
+  this.stringCount = 3;
   this.xVel = Math.random() > 0.5 ? Math.random() + 1 : Math.random() - 1;
   this.yVel = Math.random() > 0.5 ? Math.random() + 1 : Math.random() - 1;
   this.move = () => {
@@ -36,7 +46,7 @@ function Dot(initialX, initialY, debug = undefined) {
     this.y += this.yVel;
 
     matrix.dots.forEach(dot => {
-      if (this.nachbarn.length < 3 && this !== dot && !this.nachbarn.includes(dot)) {
+      if (this.nachbarn.length < this.stringCount && this !== dot && !this.nachbarn.includes(dot)) {
         this.nachbarn.push(dot);
       } else if (this !== dot && !this.nachbarn.includes(dot)) {
         this.nachbarn.sort((a, b) => {
@@ -48,10 +58,6 @@ function Dot(initialX, initialY, debug = undefined) {
         }
       }
     });
-
-    if (this.x > innerWidth || this.x < 0 || this.y < 0 || this.y > innerHeight) {
-      this.reset();
-    }
   };
 
   this.show = () => {
@@ -63,6 +69,11 @@ function Dot(initialX, initialY, debug = undefined) {
       line(this.x, this.y, dot.x, dot.y);
     });
   };
+  this.autoReset = () => {
+    if (this.x > innerWidth || this.x < 0 || this.y < 0 || this.y > innerHeight) {
+      this.reset();
+    }
+  };
 
   this.reset = () => {
     this.x = Math.floor(Math.random() * innerWidth);
@@ -73,13 +84,16 @@ function Dot(initialX, initialY, debug = undefined) {
 function Matrix(amount = 0) {
   this.dots = [];
   for (let i = 0; i < amount; i++) {
-    this.dots.push(new Dot(Math.floor(Math.random() * innerWidth), Math.floor(Math.random() * innerHeight)));
+    this.dots.push(new Dot(Math.floor(Math.random() * innerWidth), Math.floor(Math.random() * innerHeight), i, null));
   }
 
   this.showAll = () => {
     this.dots.forEach(dot => {
       dot.move();
       dot.show();
+    });
+    this.dots.forEach(dot => {
+      dot.autoReset();
     });
   };
 
